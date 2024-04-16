25cea6d4a311a662a8c463fc5a8bf368dde4f236
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-16 22:30:01 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -22,8 +22,12 @@ module tb ();
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
 
+  reg increase_duty;
+  reg decrease_duty;
+  wire PWM_OUT;
+
   // Replace tt_um_factory_test with your module name:
-  tb_PWM_Generator_Verilog user_project (
+  tt_um_Ziyi_Yuchen user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
@@ -38,7 +42,94 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .rst_n  (rst_n),     // not reset
+
+      .increase_duty(increase_duty),
+	   .decrease_duty(decrease_duty),
+	   .PWM_OUT(PWM_OUT)
+     
   );
+
+    initial begin
+ clk = 0;
+ forever #5 clk = ~clk;
+ end
+ initial begin
+  increase_duty = 0;
+  decrease_duty = 0;
+  reset = 0; // initialize reset to low
+  #100;
+	increase_duty = 1;
+  #100;
+	increase_duty = 0;
+  #100;
+	increase_duty = 1; 
+  #100;
+	increase_duty = 0;
+  #100;
+	increase_duty = 1;
+  #100;
+	increase_duty = 0;
+
+  #100;
+	decrease_duty = 1;
+  #100;
+	decrease_duty = 0;
+  #100;
+	decrease_duty = 1;
+  #100;
+	decrease_duty = 0;
+  #100;
+	decrease_duty = 1;
+  #100;
+	decrease_duty = 0;
+
+//Debouncing test
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+  #5;
+	increase_duty = 1;
+  #5;
+	increase_duty = 0;
+
+//Keep pressing test
+
+  #100;
+	increase_duty = 1;
+  #900;
+	increase_duty = 0;
+
+  #100;
+  reset = 1; // set reset high to reset the program
+  #100;
+  reset = 0; // set reset low to allow the program to run normally
+  end
+
 
 endmodule
