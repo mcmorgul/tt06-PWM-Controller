2e8b3c37d2a3adca6414a620f5dd46d88b29bf33
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-16 22:43:21 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -38,7 +38,7 @@ module tb ();
       .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n),     // not reset
+      .rst_n  (rst_n)     // not reset
   );
 
 endmodule
