0f21aa53f8abcde2d83b98d282b33f186b0b097a
Yuchen1203 <161077993+Yuchen1203@users.noreply.github.com>
2024-04-17 09:25:02 +0100

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,53 +8,6 @@ from cocotb.triggers import ClockCycles
 from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
 from cocotb.binary import BinaryValue
 
-
-@cocotb.test()
-async def test_loopback(dut):
-    dut._log.info("Start")
-
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-
-    # Reset
-    dut._log.info("Reset")
-    dut.ena.value = 1
-
-    # ui_in[0] == 0: Copy bidirectional pins to outputs
-    dut.ui_in.value = 0b0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-
-    for i in range(256):
-        dut.uio_in.value = i
-        await ClockCycles(dut.clk, 1)
-        assert dut.uo_out.value == i
-
-
-@cocotb.test()
-async def test_counter(dut):
-    dut._log.info("Start")
-
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-
-    # ui_in[0] == 1: bidirectional outputs enabled, put a counter on both output and bidirectional pins
-    dut.ui_in.value = 0b1
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
-    dut.rst_n.value = 1
-    await ClockCycles(dut.clk, 2)
-"""
-    dut._log.info("Testing counter")
-    for i in range(256):
-        assert dut.uo_out.value == dut.uio_out.value
-        assert dut.uo_out.value == i
-        await ClockCycles(dut.clk, 1)
-"""
-
 @cocotb.test()
 async def test_pwm_with_reset_and_timing(dut):
     """Test PWM output with reset behavior and check timing sensitivity to button presses."""
